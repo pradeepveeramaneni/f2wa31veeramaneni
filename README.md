d5937cf22b98827dd3c6024eda44252cb752e829
pradeepveeramaneni <pradeepraov26@gmail.com>
2022-10-26 17:23:04 -0500

    added end point bonus

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # f2wa31veeramaneni
 welcome to github repository
+[hosted at]  : https://f2wa31veeramaneni.onrender.com
--- routes/computation.js
+++ routes/computation.js
@@ -3,20 +3,20 @@ var router = express.Router();
 
 
 router.get('/',function(req,res,next) {
-    var a = Math.random();
+    var y = Math.random();
     var x;
     x = req.query.x;
     if(x==undefined)
     {
-        x=a;
+        x=y;
     }
 
-  res.render('computation', { title: `f(${x}) is ${Math.log(x)}` });
+  res.render('computation', { values: `f(${x}) is ${Math.log(x)}` });
 })
 
 /* GET home page. */
 router.get(`/`,function(req,res,next){
-res.render('computation', { title: `f(${x}) is ${Math.log(x)}` });
+res.render('computation', { values: `f(${x}) is ${Math.log(x)}` });
 res.end();
 });
 
