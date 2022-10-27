5bf88c48c0767cd14ebbc5544ddc91845d8bd073
pradeepveeramaneni <pradeepraov26@gmail.com>
2022-10-26 20:34:53 -0500

    end point added bonus1

--- routes/computation.js
+++ routes/computation.js
@@ -12,12 +12,6 @@ router.get('/',function(req,res,next) {
     }
 
   res.render('computation', { values: `f(${x}) is ${Math.log(x)}` });
-})
-
-/* GET home page. */
-router.get(`/`,function(req,res,next){
-res.render('computation', { values: `f(${x}) is ${Math.log(x)}` });
-res.end();
 });
 
 module.exports = router;
